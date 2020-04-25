03ab04d4e2f5e0b53aaf03784abbc5460e15f440
SekarAyuNur <62018401+SekarAyuNur@users.noreply.github.com>
2020-04-25 21:21:40 +0700

    Add files via upload

---
+++ uts_Out_SekarAyuNurohmah.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+	<title>Sekar Ayu Nurohmah / 171011401314</title>
+</head>
+<body>
+<P align="center">Data Pemantaun Covid19 wilayah Jawa Barat"</p>
+<P align="center">Per 18 April 2020 21:38:16 (24 April 2020, 20.42 WIB.)</p>
+<P align="center">Sekar Ayu Nurohmah / 171011401314</p>
+<input type="submit" name="submit" value="proses">
+					<table border="1">			
+		<tr>
+		<td width="10%"> Jumlah Positif</td>
+		<td width="10%"> Jumlah Dirawat</td>
+		<td width="10%"> Jumlah Sembuh</td>
+		<td width="10%"> Jumlah Meninggal</td>
+		
+		</tr>
+		
+		<tr>
+		<td>641</td>
+		<td>229</td>
+		<td>41</td>
+		<td>56</td>
+		
+		</tr>
+	</table>
+	</form>
+</body>
+</html>
